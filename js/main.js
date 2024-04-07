a53812c10810188ca130c7d045ea6fe0eae44868
NuraddinMammadzada <nmammadzada64@gmail.com>
2024-04-07 17:39:30 +0400

    azsxdcf

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 
 const cars = [
-    { brand: "Toyota", model: "Corolla", year: 2018, price: 25000, probeg: 125000, image: "toyota_corolla.jpg" },
-    { brand: "Honda", model: "Civic", year: 2019, price: 27000,probeg: 125000, image: "honda_civic.jpg" },
-    { brand: "Ford", model: "Fusion", year: 2017, price: 20000,probeg: 125000, image: "ford_focus.jpg" },
-    { brand: "Mercedes", model: "E200", year: 2018, price: 25000,probeg: 48000, image: "E200.jpg" },
-    { brand: "Bmw", model: "Civic", year: 2019, price: 27000,probeg: 125000, image: "honda_civic.jpg" },
-    { brand: "Nissan", model: "Fusion", year: 2017, price: 20000,probeg: 125000, image: "ford_focus.jpg" },
-    { brand: "Mercedes-Amg", model: "Corolla", year: 2018, price: 25000,probeg: 125000, image: "toyota_corolla.jpg" },
-    { brand: "Mercedes-Maybach", model: "Civic", year: 2019, price: 27000,probeg: 125000, image: "honda_civic.jpg" },
-    { brand: "Mercedes", model: "Fusion", year: 2017, price: 20000,probeg: 125000, image: "ford_focus.jpg" },
-    { brand: "Bmw-Alpina", model: "Corolla", year: 2018, price: 25000,probeg: 125000, image: "toyota_corolla.jpg" },
-    { brand: "Porche", model: "Civic", year: 2019, price: 27000,probeg: 125000, image: "honda_civic.jpg" },
-    { brand: "RangeRover", model: "Fusion", year: 2017, price: 20000,probeg: 125000, image: "ford_focus.jpg" }
+    { brand: "Toyota", model: "Corolla", year: 2018, price: 27000, probeg: 15000, image: "toyota_corolla.jpg" },
+    { brand: "Honda", model: "Civic", year: 2019, price: 43000,probeg: 50000, image: "honda_civic.jpg" },
+    { brand: "Ford", model: "Fusion", year: 2014, price: 18700,probeg: 225000, image: "ford_focus.jpg" },
+    { brand: "Mercedes", model: "E200", year: 2013, price: 35700,probeg: 48756, image: "E200.jpg" },
+    { brand: "Bmw", model: "M5F90", year: 2019, price: 122000,probeg: 25000, image: "F90.jpg" },
+    { brand: "Nissan", model: "Skylyne-R34", year: 2003, price: 40000,probeg: 325000, image: "r34.jpg" },
+    { brand: "Mercedes", model: "CLS63-AMG-Restyle", year: 2015, price: 63700,probeg: 125041, image: "cattouch.jpg" },
+    { brand: "Mercedes-Maybach", model: "S600", year: 2019, price: 340000,probeg: 5000, image: "S600.jpg" },
+    { brand: "Mercedes", model: "E63amg", year: 2018, price: 70000,probeg: 65450, image: "E63.jpg" },
+    { brand: "Bmw-Alpina", model: "M5f10", year: 2012, price: 55000,probeg: 225000, image: "M5F10.jpg" },
+    { brand: "Porche", model: "Cayenne", year: 2014, price: 65000,probeg: 109430, image: "Cayenne.jpg" },
+    { brand: "RangeRover", model: "Sport", year: 2015, price: 78000,probeg: 75471, image: "RRsport.jpg" }
 
 ];
 
@@ -20,7 +20,6 @@ function createCarCard(car) {
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
-    <aside>
         <img src="images/${car.image}" alt="${car.brand} ${car.model}">
         <h2>${car.brand} ${car.model}</h2>
         <p><strong>IL:</strong> ${car.year}</p>
